45a5584b4b6c6f09eacdce0f82e3f33074c1e555
Rovshan Rasulov <soulofchinder.2@gmail.com>
2025-11-24 23:44:13 +0400

    first commit

---
+++ Stax/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Stax
+//
+//  Created by Rovshan Rasulov on 24.11.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
