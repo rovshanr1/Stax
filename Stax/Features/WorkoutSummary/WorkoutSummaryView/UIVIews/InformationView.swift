bc96dd57ba5b13d5992519abe90a38b584bd2b9f
Rovshan Rasulov <soulofchinder.2@gmail.com>
2026-02-04 20:46:01 +0400

    refactor(ui): replace DividerCell with internal separatorView

--- Stax/Features/WorkoutSummary/WorkoutSummaryView/UIVIews/InformationView.swift
+++ Stax/Features/WorkoutSummary/WorkoutSummaryView/UIVIews/InformationView.swift
@@ -8,6 +8,93 @@
 import UIKit
 
 class InformationView: UIView {
-
-
+    
+    private let duration: UILabel = {
+        let label = UILabel()
+        label.textColor = .secondaryLabel
+        label.font = .systemFont(ofSize: 12, weight: .regular)
+        label.text = "Duration"
+        return label
+    }()
+    
+    private let volume: UILabel = {
+        let label = UILabel()
+        label.textColor = .secondaryLabel
+        label.font = .systemFont(ofSize: 12, weight: .regular)
+        label.text = "Volume"
+        return label
+    }()
+    
+    private let sets: UILabel = {
+        let label = UILabel()
+        label.textColor = .secondaryLabel
+        label.font = .systemFont(ofSize: 12, weight: .regular)
+        label.text = "Sets"
+        return label
+    }()
+    
+    private var timer: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.font = .systemFont(ofSize: 16, weight: .semibold)
+        label.text = "0s"
+        return label
+    }()
+    
+    private var volumeLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.font = .systemFont(ofSize: 16, weight: .semibold)
+        label.text = "0 kg"
+        return label
+    }()
+    
+    private var setsLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.font = .systemFont(ofSize: 16, weight: .semibold)
+        label.text = "0"
+        return label
+    }()
+    
+   
+    
+    private lazy var labelStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [duration, volume, sets])
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.distribution = .fillEqually
+        stackView.spacing = 4
+        return stackView
+    }()
+    
+    private lazy var valueStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [timer, volumeLabel, setsLabel])
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.distribution = .fillEqually
+        stackView.spacing = 4
+        return stackView
+    }()
+    
+    private lazy var mainStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [labelStackView, valueStackView])
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        return stackView
+    }()
+    
+    private let seperatorView: UIView = {
+        let view  = UIView()
+        view.backgroundColor = .separator
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
